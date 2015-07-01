b7c8f60622581a2df15e468cdae026da2c2293ba
istobarton <istobarton@gmail.com>
2015-06-30 19:32:56 -0500

    refactored code w/ express library

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,41 +1,75 @@
 /* Import node's http module: */
-var http = require("http");
+/*var http = require("http");
 var handleRequest = require('./request-handler').requestHandler;
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
 var port = 3000;
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
 var ip = "127.0.0.1";
 
-
-
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
 var server = http.createServer(handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+server.listen(port, ip);*/
+var express = require('express');
+var app = express();
+var messages = [];
+var defaultCorsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Cccess-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "X-Parse-Application-Id, X-Parse-REST-API-Key, content-type, accept",
+  "Access-Control-Max-age": 10 // Seconds.
+};
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
+var headers = defaultCorsHeaders;
 
+app.get('/classes/messages', function(req, res){
+  headers['Content-Type'] = 'application/json'
+  res.writeHead(200, headers);
+  res.end(JSON.stringify({results: messages}));
+
+});
+
+app.get('/classes/room1', function(req, res){
+  res.writeHead(200, headers);
+  res.end(JSON.stringify({results: messages}));
+
+});
+
+app.post('/classes/messages', function(req, res){
+  var data = '';
+  res.writeHead(201, headers);
+  req.on('data', function(chunk){
+    data += chunk;
+  });
+  req.on('end', function(){
+    messages.push(JSON.parse(data))
+    res.end(data);
+  });
+
+});
+app.post('/classes/room1', function(req, res){
+  var data = '';
+  res.writeHead(201, headers);
+  req.on('data', function(chunk){
+    data += chunk;
+  });
+  req.on('end', function(){
+    messages.push(JSON.parse(data))
+    res.end(data);
+  });
+});
+
+app.options('/classes/messages', function(req, res){
+  res.writeHead(200, headers);
+  res.end(JSON.stringify(headers));
+
+});
+
+app.options('/classes/room1', function(req, res){
+  res.writeHead(200, headers);
+  res.end(JSON.stringify(headers));
+
+});
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000...')
+});
